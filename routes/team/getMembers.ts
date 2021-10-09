160c3ee5f7118cbb591968cffd221445c87da15d
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-10-10 04:47:03 +0530

    Added get member route

--- routes/team/getMembers.ts
+++ routes/team/getMembers.ts
@@ -1,11 +1,29 @@
 import {Request, Response} from "express";
 import User from "../../models/user";
 
-// /api/team/add PUT
+// /api/team/add GET
 const getMembers = async (req: Request, res: Response) => {
-  const {teamId} = req.body
-  res.send({
-    success: false
+  const {teamId, idOnly} = req.query
+  if(!teamId)
+    return res.send({
+      success: false,
+      message: "Provide the team id"
+    })
+  const members = await User.findAll({where:{teamId}})
+  const memberList = members.map((member:any) => {
+    const memberData = member?.dataValues
+    if(idOnly)
+      return memberData?.userId
+    return {
+      userId: memberData.userId,
+      name: memberData.name,
+      email: memberData.email,
+      roles: JSON.parse(memberData.roles),
+    }
+  })
+  return res.send({
+    success: true,
+    members: memberList
   })
 }
 
