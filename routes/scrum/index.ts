48ef2e5ec55810fd0fa58919ac61f447192c9b42
PiyushNadoda <910piyushsindhav@gmail.com>
2021-10-10 01:56:20 +0530

    added view scrum

--- routes/scrum/index.ts
+++ routes/scrum/index.ts
@@ -5,11 +5,14 @@ import scrumInitialChecks from "../../middlewares/scrumInitialChecks";
 import edit from "./edit"
 import attendance from "./attendance";
 import isAllowedToGiveAttendace from "../../middlewares/attendancePerm";
+import view from "./view";
 
 const router = express.Router();
 
 router.post("/set", scrumInitialChecks, add);
 router.put("/set", edit)
 router.post("/attendance",isAllowedToGiveAttendace, attendance)
+router.get("/view", view)
+
 
 export default router;
