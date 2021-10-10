b00829d5e740f81a9609c890421d5ef78162b3a9
tanmayVaish <tanmay777leon@gmail.com>
2021-10-11 00:50:21 +0530

    view BL added

---
+++ routes/team/viewBatchLeader.ts
@@ -0,0 +1,28 @@
+import {Response} from "express";
+import Team from "../../models/team";
+
+
+const viewBatchLeader = async (req: any, res: Response) => {
+
+  const {teamId} = req.query;
+
+  const data: any = await Team.findOne({where: {teamId}});
+
+  if (!data)
+    return res.send({
+      success: false,
+      message: "No team found!"
+    })
+  if (data.batchLeader)
+    return res.send({
+      success: true,
+      bl: data.batchLeader,
+      message: "batch leader successfully fetched!"
+    })
+  res.send({
+    success: true,
+    message: "No batch leader is assigned for this team!"
+  })
+}
+
+export default viewBatchLeader;
