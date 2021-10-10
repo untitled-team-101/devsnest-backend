d36fc736a09afe6eee7db1301bbb3ab745a6a914
PiyushNadoda <910piyushsindhav@gmail.com>
2021-10-10 22:51:57 +0530

    added role when asigning batchleader

--- routes/team/addBatchLeader.ts
+++ routes/team/addBatchLeader.ts
@@ -1,26 +1,38 @@
-import {Response} from "express";
+import { Response } from "express";
 import Team from "../../models/team";
+import User from "../../models/user";
+import Roles from "../../utils/roles";
 
 const addBatchLeader = async (req: any, res: Response) => {
+  const { userId, teamId } = req.body;
 
-  const {userId, teamId} = req.body;
+  const updatedData = await Team.update(
+    { batchLeader: userId },
+    { where: { teamId: teamId } }
+  );
 
-  const updatedData = await Team.update({batchLeader: userId}, {where: {teamId:teamId}});
+  console.log(updatedData);
 
-  console.log(updatedData)
-
-  if(updatedData[0] > 0){
+  if (updatedData[0] > 0) {
+    const user: any = await User.findOne({ where: { userId } });
+    if (user.roles.indexOf(Roles.bl) === -1) {
+      const newRoles = JSON.parse(user.roles);
+      newRoles.push(Roles.bl);
+      await User.update(
+        { roles: JSON.stringify(newRoles) },
+        { where: { userId: userId } }
+      );
+    }
     res.send({
       success: true,
-      message: `${userId} is assigned as Batch Leader of ${teamId}`
-    })
-  }
-  else {
+      message: `${userId} is assigned as Batch Leader of ${teamId}`,
+    });
+  } else {
     res.send({
-      success:false,
-      message: "Team/User not found!"
-    })
+      success: false,
+      message: "Team/User not found!",
+    });
   }
-}
+};
 
 export default addBatchLeader;
