ba04a1df649df3112cb491784212db81c3aa33ac
PiyushNadoda <910piyushsindhav@gmail.com>
2021-10-10 22:06:35 +0530

    Added check access middleware, get list of users, edit user roles,delete team, remove team member

--- routes/user/list.ts
+++ routes/user/list.ts
@@ -1,11 +1,46 @@
-import {Request, Response} from "express"
-
+import { Request, Response } from "express";
+import User from "../../models/user";
 // GET /api/user/list
-const list = async (req:Request, res:Response) => {
-  const {search} = req.query
-  res.send({
-    success: false
-  })
-}
+const list = async (req: Request, res: Response) => {
+  const { userid, name, email, role } = req.query;  
+  if (Object.keys(req.query).length !== 0) {
+    if (userid) {
+      const users = await User.findOne({ where: { userid } });
+      res.send({
+        data: users,
+        success: false,
+      });
+    } else if (name) {
+      const users = await User.findAll({ where: { name } });
+      res.send({
+        data: users,
+        success: false,
+      });
+    } else if (email) {
+      const users = await User.findAll({ where: { email } });
+      res.send({
+        data: users,
+        success: false,
+      });
+    // } else if (role) {
+    //   const users = await User.findAll({ where: {roles: role} });
+    //   res.send({
+    //     data: users,
+    //     success: false,
+    //   });
+    } else {
+      res.status(200).send({
+        messsage: "Invalid Query",
+        success: false,
+      });
+    }
+  } else {
+    const users = await User.findAll();
+    res.send({
+      data: users,
+      success: false,
+    });
+  }
+};
 
 export default list;
--- routes/user/role.ts
+++ routes/user/role.ts
@@ -1,11 +1,42 @@
 import {Request, Response} from "express"
+import { STRING } from "sequelize";
+import User from "../../models/user"
 
 // PUT /api/user/role
-const role = async (req:Request, res:Response) => {
-  const {userID, roles} = req.body()
-  res.send({
-    success: false
-  })
-}
+const role = async (req: any, res:Response) => {
+  console.log(req.body);
+  const {userId, roles, action} = req.body;
+  const user : any = await User.findOne({where:{userId}})
+  if(action === "ADD"){
+  if(JSON.parse(user.roles).indexOf(roles) > -1){
+    res.status(200).json({
+      message: "role already exist",
+      success: false
+    })
+  }else{
+    const newRoles = JSON.parse(user.roles);
+    newRoles.push(roles)
+    await User.update({roles : JSON.stringify(newRoles)},{where:{userId : userId}})
+    res.status(200).json({
+      message : "role updated successfully",
+      success: true
+    })
+  }}
+  if(action === "REMOVE"){
+    if(JSON.parse(user.roles).indexOf(roles) === -1){
+      res.status(200).json({
+        message: "role doesn't already exist",
+        success: false
+      })
+    }else{
+      var newRoles = JSON.parse(user.roles);
+      newRoles = newRoles.find((role : String) => role !== roles)
+      await User.update({roles : JSON.stringify(newRoles)},{where:{userId : userId}})
+      res.status(200).json({
+        message : "role removed successfully",
+        success: true
+      })
+    }}
+  }
 
 export default role;
